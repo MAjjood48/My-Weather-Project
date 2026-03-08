f9ccd540e2bee6d426df4261fbe98bd2d2891528
Youssef_Allam <ylam07049@gmail.com>
2026-03-08 22:41:11 +0200

    Fix formatting and add comments in main.jsx

--- src/Search/main.jsx
+++ src/Search/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
+// importing the main component
 import App from "./App";
+
 import "./App.css";
 
+// selecting the div where app will render - exists in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
+
+  // StrictMode helps me find problems during development - doesnt affect production
   <React.StrictMode>
+    {/* rendering the main component */}
     <App />
   </React.StrictMode>
 );
